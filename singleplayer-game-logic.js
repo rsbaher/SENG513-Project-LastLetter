5eebe0a4771f961119f0dd21054044716cc1e8b1
Olga Chitsvarina <olga.chitsvarina@gmail.com>
2018-04-05 10:58:50 -0600

    io from other js file

--- singleplayer-game-logic.js
+++ singleplayer-game-logic.js
@@ -1,7 +1,6 @@
 module.exports = {
-    test2: function() { console.log( "Hello World"); }
-}
 
-export function test(){
-    console.log( "Hello World");
-}
+    test2: function() { console.log( "Hello World");},
+    test1: function () { console.log("Bye");}
+};
+
