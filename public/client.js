d8d86c3a5a6c4f9539712f7320d68bb51e17882e
Marc Fichtel <marc.a.fichtel@gmail.com>
2018-04-07 19:33:54 -0600

    Clean up master

--- public/client.js
+++ public/client.js
@@ -1,7 +1,7 @@
 //======================================================================================================================
 //FIREBASE INIT:
+// FIREBASE
 
-// TODO find a safe place to store project credentials???
 const config = {
     apiKey: "AIzaSyCuhMqPaI2QCFxZCbvp1hOcduf3w0vSyK0",
     authDomain: "seng513-project-lastletter.firebaseapp.com",
@@ -12,27 +12,206 @@ const config = {
 };
 firebase.initializeApp(config);
 
-//==================================================================================================================
-//GENERAL SET UP:
+// Set user variable on login & logout
+firebase.auth().onAuthStateChanged(function(user) {
 
-var socket = io();      // auto-discovery, allows bidirectional communication between client and a server
+    // Login
+    if(user) {
+        googleUser = user;
+        socket.emit('get user', googleUser);
+    }
+
+    //Logout
+    else {
+        googleUser = null;
+        dbUserObject = null;
+    }
+});
+
+//==================================================================================================================
+// GENERAL SET UP
+
+const socket = io();        // Web socket
+let googleUser = null;      // The google user account
+let dbUserObject = null;    // JSON Object representing the user's DB entry
 
 //=================================================================================================================
-// :
+// COOKIES
 
+/**
+ * Return available cookies
+ */
+// function returnCookies() { console.log("TODO return cookies"); }
 
-// function returnCookies() {
-//     console.log("load this page");
-//
-// }
-
-/*function setCookies(newName) {
-    Cookies.set('cookieNickNameStr', newName ,{ path: '' });
-}*/
+/**
+ * Save a cookie
+ * @param name of cookie to save
+ */
+//function setCookies(name) {
+//  Cookies.set('cookieNickNameStr', name ,{ path: '' });
+//}
 
 //======================================================================================================================
-//GET INFORMATION FROM THE SERVER:
+// ON LOAD
 
-// socket.on('update-online-users', function(numberOfOnlineUsers) {
-//     document.getElementById('number-of-online-users').innerHTML = numberOfOnlineUsers.toString();
+// When window loads, let user sign in or redirect them to home page
+window.onload = function() {
+
+    // TODO fix this mess
+
+    initProfile();
+    initUserAuth();
+    loadLoginPage();
+    initLeaderboard();
+
+    // User is already signed in
+    if (firebase.auth().currentUser != null) {
+        document.getElementById('login-button-header').disabled = true;
+        document.getElementById('log-out-button-header').disabled = false;
+    }
+
+    // No user is signed in
+    else {
+        document.getElementById('login-button-header').disabled = false;
+        document.getElementById('log-out-button-header').disabled = true;
+    }
+};
+
+//======================================================================================================================
+// FUNCTIONS
+
+/**
+ * Initialize User Authentication
+ * Assigns a handler to the login buttons
+ */
+function initUserAuth() {
+    document.getElementById("login-button-header").addEventListener('click', signIn, false);
+    document.getElementById("login-button-unauthorized-user-container").addEventListener('click', signIn, false);
+}
+
+/**
+ * Login with Google
+ */
+function signIn() {
+
+    // Disable login button while sign-in popup is open
+    document.getElementById('login-button-header').disabled = true;
+
+    // No user is logged in
+    if (firebase.auth().currentUser == null) {
+
+        // Sign in with Google via Popup
+        const google = new firebase.auth.GoogleAuthProvider();
+        google.addScope('https://www.googleapis.com/auth/contacts.readonly');
+
+        // Google popup sign up
+        firebase.auth().signInWithPopup(google)
+
+            // Success: register user
+            .then(function (result) {
+                googleUser = result.user;
+                socket.emit('login', googleUser);
+            })
+
+            // Error
+            .catch(function (error) {
+                console.log('Error: ' + error);
+                document.getElementById('login-button-header').disabled = false;
+                document.getElementById('log-out-button-header').disabled = true;
+            });
+    }
+
+    // A user is already signed in
+    else {
+        googleUser = firebase.auth().currentUser;
+        // TODO show home screen
+    }
+}
+
+/**
+ * Initialize profile page
+ */
+function initProfile() {
+    document.getElementById('log-out-button-header').addEventListener('click', logout, false);
+    //document.getElementById('editNameBtn').disabled = true;
+}
+
+/**
+ * Initialize the leaderboard
+ */
+function initLeaderboard() {
+    $('#singleplayer-leaderboard').empty();
+    $('#multiplayer-leaderboard').empty();
+    socket.emit('get leaderboard');
+}
+
+/**
+ * Set up home page with user info
+ * @param name of the active user
+ * @param highscore of active user in single player mode
+ */
+// function setupHomePage(name, highscore) {
+//     document.getElementById('name').innerText = name;
+//     document.getElementById('score').innerText = highscore;
+//     document.getElementById('editNameBtn').addEventListener('click', editUsername, false)
+//     document.getElementById('editNameBtn').disabled = false;
+// }
+
+/**
+ * Log user out
+ */
+function logout() {
+    if (firebase.auth().currentUser != null) {
+        firebase.auth().signOut();
+        document.getElementById('login-button-header').disabled = false;
+        document.getElementById('log-out-button-header').disabled = true;
+        // TODO show login page
+    }
+}
+
+/**
+ * We want  users to be able to change their username
+ * TODO only change name if no other user has chosen that name
+ */
+// function editUsername() {
+//
+//     // Prompt user for new name
+//     let newName = prompt('Please enter your new name', dbUserObject.name);
+//
+//     // Request name change from server
+//     if (newName !== '') { socket.emit('new name', dbUserObject, newName); }
+// }
+
+//======================================================================================================================
+// SOCKET EVENTS
+
+// When login is done, redirect to given page
+socket.on('login', function() {
+    document.getElementById('login-button-header').disabled = true;
+    document.getElementById('log-out-button-header').disabled = false;
+});
+
+// Server sends user data, set up home page
+socket.on('get user', function(obj) {
+    dbUserObject = obj;
+    //setupHomePage(obj.name, obj.singleHighScore);
+});
+
+// Server successfully updated user name in DB
+// socket.on('new name', function(newName) {
+//     googleUser.displayName = newName;
+//     document.getElementById('name').innerText = newName;
+//     dbUserObject.name = newName;
+//     initLeaderboard();
 // });
+
+// Server is sending a leaderboard entry
+socket.on('get leaderboard', function(single, leaderboardEntry) {
+    let userName = leaderboardEntry.name;
+    let userScoreSingle = leaderboardEntry.singleHighScore;
+    let userScoreMulti = leaderboardEntry.multiHighScore;
+
+    // Update the appropriate leaderboard
+    if (single) { $('#singleplayer-leaderboard').append('<li>' + userName + ': ' + userScoreSingle + '</li>'); }
+    else { $('#multiplayer-leaderboard').append('<li>' + userName + ': ' + userScoreMulti + '</li>'); }
+});
