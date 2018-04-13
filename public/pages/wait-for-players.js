1036370142f484777a9b705691205288f261b0a1
Olga Chitsvarina <olga.chitsvarina@gmail.com>
2018-04-13 10:04:32 -0600

    started to create wait page

---
+++ public/pages/wait-for-players.js
@@ -0,0 +1,55 @@
+
+//===============================================================================================================
+// JQUERY WRAP:
+
+$(function() {
+
+
+    // =========================================================================================================
+    // BUTTONS REGISTRATION:
+
+    $('#back-to-home-button-single').on('click', backButtonWasClicked);
+    $('#save-game-button-single').on('click', saveGame);
+
+
+    //==========================================================================================================
+    // PRIVATE HTML HANDLERS:
+
+    function backButtonWasClicked(){
+        loadHomePage();
+        deleteGame();
+    }
+
+    //==========================================================================================================
+    // SEND MESSAGES TO A SERVER:
+
+
+    //===========================================================================================================
+    // LISTEN TO A SERVER:
+
+
+});
+
+//==============================================================================================================
+// GLOBAL HTML EVENTS HANDLERS:
+
+function loadSinglePlayerPage() {
+
+    $('.unauthorized').hide();
+    $('.profile').hide();
+    $('.home').hide();
+    $('.multi-player').hide();
+    $('.default').show();
+    $('.authorized').show();
+    $('.single-player').show();
+    document.getElementById("single-player-messages").innerHTML = "";
+}
+
+function startSinglePlayerGame() {
+    loadSinglePlayerPage();
+    socket.emit('single-player-start-game', category, dbUserObject);
+}
+
+
+
+
