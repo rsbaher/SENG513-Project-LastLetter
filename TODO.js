62ac9edb5516e52b790ab6cfefe5a3bba6dd8be0
Marc Fichtel <marc.a.fichtel@gmail.com>
2018-04-15 12:16:16 -0600

    update totos

--- TODO.js
+++ TODO.js
@@ -1,24 +1,17 @@
 
 //=======================================================================================================
-// TODO STILL HAVE TO BE DONE:
+// NICE TO HAVE / FUTURE FEATURES:
 
 
 
-// TODO Implement multiplayer
 
-// TODO style, who is doing that?
+// TODO better styling
+// TODO responsiveness
 
-// TODO Enabling of buttons should be on load home
-
-// TODO how to make sure that one person have not been chosen by multiple players?
-// TODO boolean available??????
-
-
-  // TODO remove user from wait list in multiplayer if he exits
+// TODO make sure that one person has not been chosen by multiple players
 
 // TODO may need to change later that can get a user obj if close the window and open again (Index.js emailToGame)
 
-// TODO dictionary email to userObj
 // TODO what if user opens window in two tabs???
 // TODO authorized user is on home page, he opens another link in his browser,
 // TODO then comes back, he sees a login page, but should see a page from which he left a game
@@ -32,3 +25,7 @@
 // TODO save, load game
 // TODO When delete a single player game, make sure to check that highest score is updated in the database if necessary
 // TODO don't show login page if a user did not log out --> Done but slow (login page displays for a bit before home page loads) --> cookies might be faster (probably)
+// TODO Implement multiplayer
+// TODO Enabling of buttons should be on load home
+// TODO remove user from wait list in multiplayer if he exits
+// TODO dictionary email to userObj
